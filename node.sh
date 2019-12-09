84d817628ce53483abd856fb55c6afe96738dbe6
Tony Dan <55903286+tonydan6324@users.noreply.github.com>
2019-12-09 22:28:52 +0800

    增加优化系统内核参数
    增加优化系统内核参数

--- node.sh
+++ node.sh
@@ -59,6 +59,33 @@ check_system(){
 	exit 0;
 	fi
 }
+optimize(){
+	echo "fs.file-max = 51200" > /etc/sysctl.conf
+	echo "net.core.rmem_max = 67108864" >> /etc/sysctl.conf
+	echo "net.core.wmem_max = 67108864" >> /etc/sysctl.conf
+	echo "net.core.netdev_max_backlog = 250000" >> /etc/sysctl.conf
+	echo "net.core.somaxconn = 65535" >> /etc/sysctl.conf
+	echo "net.ipv4.tcp_syncookies = 1" >> /etc/sysctl.conf
+	echo "net.ipv4.tcp_tw_reuse = 1" >> /etc/sysctl.conf
+	echo "net.ipv4.tcp_tw_recycle = 0" >> /etc/sysctl.conf
+	echo "net.ipv4.tcp_fin_timeout = 30" >> /etc/sysctl.conf
+	echo "net.ipv4.tcp_keepalive_time = 1200" >> /etc/sysctl.conf
+	echo "net.ipv4.ip_local_port_range = 10000 65000" >> /etc/sysctl.conf
+	echo "net.ipv4.tcp_max_syn_backlog = 8192" >> /etc/sysctl.conf
+	echo "net.ipv4.tcp_max_tw_buckets = 5000" >> /etc/sysctl.conf
+	echo "net.ipv4.tcp_fastopen = 3" >> /etc/sysctl.conf
+	echo "net.ipv4.tcp_rmem = 4096 87380 67108864" >> /etc/sysctl.conf
+	echo "net.ipv4.tcp_wmem = 4096 65536 67108864" >> /etc/sysctl.conf
+	echo "net.ipv4.tcp_mtu_probing = 1" >> /etc/sysctl.conf
+	echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+	echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+	echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+	echo "* soft nofile 65535" > /etc/security/limits.conf
+	echo "* hard nofile 65535" >> /etc/security/limits.conf
+	echo "* soft nproc 65535" >> /etc/security/limits.conf
+	echo "* hard nproc 65535" >> /etc/security/limits.conf
+	sysctl -p
+}
 node_install_start(){
 	yum -y groupinstall "Development Tools"
 	yum install unzip zip git iptables -y
@@ -169,6 +196,9 @@ cd /root && rm -rf libsodium*
 echo 'iptables-restore /etc/sysconfig/iptables' >> /etc/rc.local
 chmod +x /etc/rc.d/rc.local && chmod +x /etc/rc.local
 cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime -r >/dev/null 2>&1
+clear
+echo -e "${GreenBG} 正在优化系统内核参数中...请稍后... ${Font}"
+optimize
 clear
 echo -e "${GreenBG} 将后端写入服务中中...请稍后... ${Font}"
 sleep 2
