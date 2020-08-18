9c7ba31b2d93307e9c7f2622fcdb14be1faf2737
Guccen <60386727+Guccen@users.noreply.github.com>
2020-08-18 15:28:47 +0800

    ssrmu 2.0 Bete
    ssrmu 2.0 Bete

--- ssrmu.sh
+++ ssrmu.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-#个人使用的对接脚本
+#厂长个人对接脚本 谢绝传播 
+# Version：2.0 Bete 
+# Updata time：2020-8-18 14:36:17
 #check root
 [ $(id -u) != "0" ] && { echo "错误: 您必须以root用户运行此脚本"; exit 1; }
-rm -rf node*
+rm -rf ssrmu*
 #常规变量设置
 #fonts color
 Green="\033[32m" 
@@ -35,6 +37,7 @@ get_ip(){
 	[[ -z $ip ]] && echo -e "\n 这小鸡鸡还是割了吧！\n" && exit
 }
 check_system(){
+	clear
 	if [[ -f /etc/redhat-release ]]; then
 		release="centos"
 	elif cat /etc/issue | grep -q -E -i "debian"; then
@@ -62,6 +65,7 @@ check_system(){
 	fi
 }
 optimize(){
+	clear
 	echo "fs.file-max = 51200" > /etc/sysctl.conf
 	echo "net.core.rmem_max = 67108864" >> /etc/sysctl.conf
 	echo "net.core.wmem_max = 67108864" >> /etc/sysctl.conf
@@ -89,6 +93,7 @@ optimize(){
 	sysctl -p
 }
 node_install_start_for_centos(){
+	clear
 	yum -y groupinstall "Development Tools"
 	yum install unzip zip ntpdate git iptables -y
 	yum update nss curl iptables -y
@@ -111,6 +116,7 @@ node_install_start_for_centos(){
 	cp config.json user-config.json
 }
 node_install_start_for_debian(){
+	clear
 	apt-get update -y
 	apt-get install git curl ntpdate iptables unzip zip build-essential -y
 	wget --no-check-certificate https://download.libsodium.org/libsodium/releases/libsodium-1.0.18.tar.gz
@@ -129,7 +135,7 @@ node_install_start_for_debian(){
 	cp apiconfig.py userapiconfig.py
 	cp config.json user-config.json
 }
-api(){
+api_new(){
     clear
 	echo -e "如果以下手动配置错误，请在${config}手动编辑修改"
 	read -p "请输入你的对接域名或IP(例如:http://www.baidu.com 默认为本机对接): " WEBAPI_URL
@@ -155,9 +161,46 @@ api(){
 	MU_SUFFIX=${MU_SUFFIX:-"microsoft.com"}
 	sed -i '/MU_SUFFIX/c \MU_SUFFIX = '\'${MU_SUFFIX}\''' ${config}
 }
-db(){
+api_old(){
+	clear
+	echo -e "如果以下手动配置错误，请在${config}手动编辑修改"
+	read -p "请输入新增节点目录名(如ssrmu,不可为纯数字): " NODE_LIST
+	read -p "请输入你的对接域名或IP(例如:http://www.baidu.com 默认为本机对接): " WEBAPI_URL
+	read -p "请输入muKey(在你的配置文件中 默认marisn):" WEBAPI_TOKEN
+	read -p "请输入你的节点编号(回车默认为节点ID 3):  " NODE_ID
+	read -p "请输入你的混淆参数[务必与配置文件中一致](回车默认为: microsoft.com):  " MU_SUFFIX
+	NODE_LIST=${NODE_LIST:-"ssrmu"} #默认为ssrmu
+	git clone ${Github} "/root/${NODE_LIST}"
+	if [ ! -d "/root/${NODE_LIST}" ]; then
+		echo -e "${Error} 下载资源失败，请检查是否安装Git"
+		exit 1
+	fi
+	cd "/root/${NODE_LIST}"
+	pip install -r requirements.txt
+	pip install cymysql
+	cp apiconfig.py userapiconfig.py
+	cp config.json user-config.json
+	cp ssr.service ${NODE_LIST}.service
+	echo -e "modify Config.py...\n"
+	get_ip
+	WEBAPI_URL=${WEBAPI_URL:-"http://${ip}"}
+	sed -i '/WEBAPI_URL/c \WEBAPI_URL = '\'${WEBAPI_URL}\''' ${config}
+	#sed -i "s#https://zhaoj.in#${WEBAPI_URL}#" /root/shadowsocks/userapiconfig.py
+	WEBAPI_TOKEN=${WEBAPI_TOKEN:-"marisn"}
+	sed -i '/WEBAPI_TOKEN/c \WEBAPI_TOKEN = '\'${WEBAPI_TOKEN}\''' ${config}
+	#sed -i "s#glzjin#${WEBAPI_TOKEN}#" /root/shadowsocks/userapiconfig.py
+	NODE_ID=${NODE_ID:-"3"}
+	sed -i '/NODE_ID/c \NODE_ID = '${NODE_ID}'' ${config}
+	MU_SUFFIX=${MU_SUFFIX:-"microsoft.com"}
+	sed -i '/MU_SUFFIX/c \MU_SUFFIX = '\'${MU_SUFFIX}\''' ${config}
+	#替换守护程序
+	sed -i "s/ssr/${NODE_LIST}/" ${NODE_LIST}.service
+	sed -i "s/shadowsocks/${NODE_LIST}/" ${NODE_LIST}.service
+}
+db_new(){
     clear
 	echo -e "如果以下手动配置错误，请在${config}手动编辑修改"
+	read -p "请输入新增节点目录名(如ssrmu,不可为纯数字): " NODE_LIST
 	read -p "请输入你的对接数据库IP(例如:127.0.0.1 如果是本机请直接回车): " MYSQL_HOST
 	read -p "请输入你的数据库名称(默认sspanel):" MYSQL_DB
 	read -p "请输入你的数据库端口(默认3306):" MYSQL_PORT
@@ -165,12 +208,18 @@ db(){
 	read -p "请输入你的数据库密码(默认root):" MYSQL_PASS
 	read -p "请输入你的节点编号(回车默认为节点ID 3):  " NODE_ID
 	read -p "请输入你的混淆参数[务必与配置文件中一致](回车默认为: microsoft.com):  " MU_SUFFIX
-	if [[ ${release} == "centos" ]];then
-	node_install_start_for_centos
-	else
-	node_install_start_for_debian
+	NODE_LIST=${NODE_LIST:-"ssrmu"} #默认为ssrmu
+	git clone ${Github} "/root/${NODE_LIST}"
+	if [ ! -d "/root/${NODE_LIST}" ]; then
+		echo -e "${Error} 下载资源失败，请检查是否安装Git"
+		exit 1
 	fi
-	cd /root/shadowsocks
+	cd "/root/${NODE_LIST}"
+	pip install -r requirements.txt
+	pip install cymysql
+	cp apiconfig.py userapiconfig.py
+	cp config.json user-config.json
+	cp ssr.service ${NODE_LIST}.service
 	echo -e "modify Config.py...\n"
 	get_ip
 	sed -i '/API_INTERFACE/c \API_INTERFACE = '\'glzjinmod\''' ${config}
@@ -188,70 +237,165 @@ db(){
 	sed -i '/NODE_ID/c \NODE_ID = '${NODE_ID}'' ${config}
 	MU_SUFFIX=${MU_SUFFIX:-"microsoft.com"}
 	sed -i '/MU_SUFFIX/c \MU_SUFFIX = '\'${MU_SUFFIX}\''' ${config}
+	#替换守护程序
+	sed -i "s/ssr/${NODE_LIST}/" ${NODE_LIST}.service
+	sed -i "s/shadowsocks/${NODE_LIST}/" ${NODE_LIST}.service
 }
-clear
-check_system
-echo -e "\033[1;5;31m请选择对接模式：\033[0m"
-echo -e "1.API对接模式"
-echo -e "2.数据库对接模式"
-read -t 30 -p "选择：" NODE_MS
-case $NODE_MS in
-		1)
-			api
-			;;
-		2)
-			db
-			;;
-		*)
-		    echo -e "请选择正确对接模式"
-			exit 1
-			;;
-esac
-if [[ ${release} == "centos" ]];then
-#关闭CentOS7的防火墙
-systemctl stop firewalld.service
-systemctl disable firewalld.service
-#iptables
-iptables -F
-iptables -X  
-iptables -I INPUT -p tcp -m tcp --dport 22:65535 -j ACCEPT
-iptables -I INPUT -p udp -m udp --dport 22:65535 -j ACCEPT
-iptables-save >/etc/sysconfig/iptables
-echo 'iptables-restore /etc/sysconfig/iptables' >> /etc/rc.local
-chmod +x /etc/rc.d/rc.local && chmod +x /etc/rc.local
-fi
-#删除libsodium
-cd /root && rm -rf libsodium*
-cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime -r >/dev/null 2>&1
-timedatectl set-timezone Asia/Shanghai
-timedatectl
-ntpdate -u cn.pool.ntp.org
-clear
-echo -e "${GreenBG} 正在优化系统内核参数中...请稍后... ${Font}"
-optimize
-echo -e "${GreenBG} 将后端写入服务中中...请稍后... ${Font}"
-sleep 2
-if [[ ${release} == "centos" ]];then
-cp /root/shadowsocks/ssr.service /usr/lib/systemd/system/ssr.service
-else
-cp /root/shadowsocks/ssr.service /lib/systemd/system/ssr.service
-fi
-systemctl daemon-reload
-systemctl start ssr
-systemctl enable ssr
-if [[ `ps -ef | grep server.py |grep -v grep | wc -l` -ge 1 ]];then
-	echo -e "${OK} ${GreenBG} 后端已启动 ${Font}"
-else
-	echo -e "${OK} ${RedBG} 后端未启动 ${Font}"
-	echo -e "请检查是否为Centos 7.x系统、检查配置文件是否正确、检查是否代码错误请反馈"
-	exit 1
-fi
-stdout() {
-    echo -e "\033[32m$1\033[0m"
+
+complete_new()
+{
+	clear
+	if [[ ${release} == "centos" ]];then
+	#关闭CentOS7的防火墙
+	systemctl stop firewalld.service
+	systemctl disable firewalld.service
+	#iptables
+	iptables -F
+	iptables -X  
+	iptables -I INPUT -p tcp -m tcp --dport 22:65535 -j ACCEPT
+	iptables -I INPUT -p udp -m udp --dport 22:65535 -j ACCEPT
+	iptables-save >/etc/sysconfig/iptables
+	echo 'iptables-restore /etc/sysconfig/iptables' >> /etc/rc.local
+	chmod +x /etc/rc.d/rc.local && chmod +x /etc/rc.local
+	fi
+	#删除libsodium
+	cd /root && rm -rf libsodium*
+	cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime -r >/dev/null 2>&1
+	timedatectl set-timezone Asia/Shanghai
+	timedatectl
+	ntpdate -u cn.pool.ntp.org
+	clear
+	echo -e "${GreenBG} 正在优化系统内核参数中...请稍后... ${Font}"
+	optimize
+	echo -e "${GreenBG} 将后端写入服务中中...请稍后... ${Font}"
+	sleep 2
+	if [[ ${release} == "centos" ]];then
+	cp /root/shadowsocks/ssr.service /usr/lib/systemd/system/ssr.service
+	else
+	cp /root/shadowsocks/ssr.service /lib/systemd/system/ssr.service
+	fi
+	systemctl daemon-reload
+	systemctl start ssr
+	systemctl enable ssr
+	if [[ `ps -ef | grep server.py |grep -v grep | wc -l` -ge 1 ]];then
+		echo -e "${OK} ${GreenBG} 后端已启动 ${Font}"
+	else
+		echo -e "${OK} ${RedBG} 后端未启动 ${Font}"
+		echo -e "请检查是否为Centos 7.x系统、检查配置文件是否正确、检查是否代码错误请反馈"
+		exit 1
+	fi
+	stdout() {
+		echo -e "\033[32m$1\033[0m"
+	}
+	stdout "启动命令：systemctl start ssr"
+	stdout "停止命令：systemctl stop ssr"
+	stdout "重启命令：systemctl restart ssr"
+	stdout "开启自启：systemctl enable ssr"
+	stdout "关闭自启：systemctl disable ssr"
+	stdout "查看状态：systemctl status ssr"
 }
-stdout "启动命令：systemctl start ssr"
-stdout "停止命令：systemctl stop ssr"
-stdout "重启命令：systemctl restart ssr"
-stdout "开启自启：systemctl enable ssr"
-stdout "关闭自启：systemctl disable ssr"
-stdout "查看状态：systemctl status ssr"
+
+complete_old()
+{
+	clear
+	if [[ ${release} == "centos" ]];then
+	cp /root/${NODE_LIST}/${NODE_LIST}.service /usr/lib/systemd/system/${NODE_LIST}.service
+	else
+	cp /root/${NODE_LIST}/${NODE_LIST}.service /lib/systemd/system/${NODE_LIST}.service
+	fi
+	systemctl daemon-reload
+	systemctl start ${NODE_LIST}
+	systemctl enable ${NODE_LIST}
+	stdout() {
+		echo -e "\033[32m$1\033[0m"
+	}
+	stdout "启动命令：systemctl start ${NODE_LIST}"
+	stdout "停止命令：systemctl stop ${NODE_LIST}"
+	stdout "重启命令：systemctl restart ${NODE_LIST}"
+	stdout "开启自启：systemctl enable ${NODE_LIST}"
+	stdout "关闭自启：systemctl disable ${NODE_LIST}"
+	stdout "查看状态：systemctl status ${NODE_LIST}"
+}
+
+choose_mode()
+{
+	clear
+	echo -e "\033[1;5;31m请选择对接类型：\033[0m"
+	echo -e "1.新节点对接"
+	echo -e "2.小鸡复用对接(必须使用本脚本已搭建过新节点才行)"
+	read -t 30 -p "选择：" MODE_MS
+	case $MODE_MS in
+			1)
+				if [ -d "/root/shadowsocks" ]; then
+					echo -e "${Error} 检测已安装节点，请选择小鸡复用对接"
+					exit 1
+				fi
+				select_mode_new
+				;;
+			2)
+				if [ ! -d "/root/shadowsocks" ]; then
+					echo -e "${Error} 检测未安装节点，请选择新节点对接"
+					exit 1
+				fi
+				select_mode_old
+				;;
+			*)
+				echo -e "请选择正确对接类型"
+				exit 1
+				;;
+	esac
+}
+select_mode_new()
+{
+	clear
+	echo -e "\033[1;5;31m请选择对接模式：\033[0m"
+	echo -e "1.API对接模式"
+	echo -e "2.数据库对接模式"
+	read -t 30 -p "选择：" NODE_MS_NEW
+	case $NODE_MS_NEW in
+			1)
+				api_new
+				complete_new
+				;;
+			2)
+				db_new
+				complete_new
+				;;
+			*)
+				echo -e "请选择正确对接模式"
+				exit 1
+				;;
+	esac
+}
+
+select_mode_old()
+{
+	clear
+	echo -e "\033[1;5;31m请选择对接模式：\033[0m"
+	echo -e "1.API对接模式"
+	echo -e "2.数据库对接模式"
+	read -t 30 -p "选择：" NODE_MS_OLD
+	case $NODE_MS_OLD in
+			1)
+				api_old
+				complete_old
+				;;
+			2)
+				db_old
+				complete_old
+				;;
+			*)
+				echo -e "请选择正确对接模式"
+				exit 1
+				;;
+	esac
+}
+
+main() {
+	check_system
+	choose_mode
+}
+
+main
+
+
